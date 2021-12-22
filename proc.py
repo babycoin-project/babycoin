55417907a0b4cf258830ecaf9efe81cccbf34ac1
synthaze <florian.malard@gmail.com>
2021-12-22 18:55:18 +0100

    None

--- proc.py
+++ proc.py
@@ -7,7 +7,7 @@ for fname in lst:
     try:
         content = read_file(fname)
 
-        if '52021' in content and 'build' not in fname and fname != 'lst':
+        if '52921' in content and 'build' not in fname and fname != 'lst':
             lines = [x for x in content.splitlines() if '5202' in x]
             for x in lines:
                 if len(x) < 100:
